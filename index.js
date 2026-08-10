ca8275e17275914ddafefffdd6ebfcfc613606d2
laxmis-prog <laxmisanuu5@gmail.com>
2026-08-10 12:13:57 +0300

    Practise datatype strings

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 
-let age = 25;
-let price = 10.99;
-let gpa = 2.1;
+//let age = 25;
+//let price = 10.99;
+//let gpa = 2.1;
 
-console.log(typeof gpa);
-console.log(`Ýou are ${age} years old`);
-console.log(`The price is $${price}`);
-console.log(`Your gpa is: ${gpa}`);
+let firstName = "sanu";
+let favoriteFood = "pizza";
+let email = "sanu1@gmail.com";
 
+console.log(typeof firstName);
+console.log(`Your name is ${firstName}`);
+console.log(`My favorite food is ${favoriteFood}`);
+console.log(`My email is ${email}`);
 
 
 
