533bec587e8b9cbb8bc85f976b937d5412a3eb71
laxmis-prog <laxmisanuu5@gmail.com>
2026-08-10 12:24:59 +0300

    Practise datatype booleans.

--- index.js
+++ index.js
@@ -3,14 +3,18 @@
 //let price = 10.99;
 //let gpa = 2.1;
 
-let firstName = "sanu";
-let favoriteFood = "pizza";
-let email = "sanu1@gmail.com";
+//let firstName = "sanu";
+//let favoriteFood = "pizza";
+//let email = "sanu1@gmail.com";
 
-console.log(typeof firstName);
-console.log(`Your name is ${firstName}`);
-console.log(`My favorite food is ${favoriteFood}`);
-console.log(`My email is ${email}`);
+let online = true;
+let forSale = false;
+let isStudent = true;
+
+console.log(`sanu is online: ${online}`);
+console.log(`Is this car for sale:${forSale}`);
+console.log(`Is sanu enrolled: ${isStudent}`);
+
 
 
 
