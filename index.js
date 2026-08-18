d894216600c2c6a8542fdda7057546d8e96f84ad
laxmis-prog <laxmisanuu5@gmail.com>
2026-08-18 10:51:57 +0300

    Practise more type conversion

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 
 
-let age = window.prompt("How old are you?");
+let x;
+let y;
+let z;
+
+x= Number (x);
+y= String (y);
+z= Boolean (z);
+
+console.log(x, typeof x);
+console.log(y, typeof y);
+console.log(z, typeof z);
 
 
-age = Number(age);
-age+=1;
-console.log(age);
  
 
 
