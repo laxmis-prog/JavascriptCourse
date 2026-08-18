e5cf018f0e6f2f7b2bed7f4992c89493e4dc6a4c
laxmis-prog <laxmisanuu5@gmail.com>
2026-08-18 10:25:11 +0300

    Practise user input professionally with javascript

--- index.js
+++ index.js
@@ -3,7 +3,14 @@
 let username;
  document.getElementById("mySubmit").onclick = function() {
    username = document.getElementById("myText").value;
-   console.log(username);
+   document.getElementById("myH1").textContent = `Hello ${username}`
+ }
+
+
+ let userEmail;
+ document.getElementById("mysend").onclick = function () {
+  userEmail = document.getElementById("myTextt").value;
+  document.getElementById("myH2").textContent = `Hello ${userEmail}`
  }
 
 
