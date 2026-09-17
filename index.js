5ba8063dab256f2fd3413c0c7291072f6b5dfc7a
laxmis-prog <laxmisanuu5@gmail.com>
2026-09-17 14:51:03 +0300

    Practise math object in javascript.

--- index.js
+++ index.js
@@ -1,18 +1,9 @@
-const decreaseBtn = document.getElementById("decreaseBtn");
-const resetBtn = document.getElementById("resetBtn");
-const increaseBtn = document.getElementById("increaseBtn");
-const countLabel = document.getElementById("countLabel");
-let count = 0;
 
-increaseBtn.onclick = function(){
-  count++;
-  countLabel.textContent = count;
-}
-decreaseBtn.onclick = function(){
-  count--;
-  countLabel.textContent = count;
-}
-resetBtn.onclick = function(){
-  count = 0;
-  countLabel.textContent = count;
-}
+let x = 1;
+let y = 2;
+let z = 3;
+
+let max = Math.max(x,y,z);
+let min = Math.min(x,y,z);
+
+console.log(min);
